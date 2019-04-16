c40fd29bcaf985735dfdccb5ff583f0c7d896143
Amy Boyd <amynic@users.noreply.github.com>
2019-04-16 18:42:58 +0100

    camera change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = { video: { facingMode: { exact :"environment", } }, audio: false };
 var track = null;
 
 // Define constants
@@ -28,7 +28,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-    // track.stop();
+    //track.stop();
 };
 
 // Start the video stream when the window loads
